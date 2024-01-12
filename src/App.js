00fb9d39bf51357235bbd4ae4b89bcc7e59d6b55
ariston.rais <ariston.zidane@m3.formulatrix.com>
2024-01-12 16:17:04 +0700

    Fist commit React App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './didoakan.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Haloo, ini adalah App React Pertamaku!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Belajar React
         </a>
       </header>
     </div>
